0cf26b0bc1ff913d3d0036fc85eb8943ac5494bf
jdzhang <jdzhang@in-road.com>
2025-07-21 17:27:23 +0800

    直接使用webhook

--- src/services/github.js
+++ src/services/github.js
@@ -14,8 +14,60 @@ class GitHubService {
 	}
 
 	// 获取提交的diff信息
-	async getCommitDiff(owner, repo, commitSha) {
+	async getCommitDiff(owner, repo, commitSha, filesFromWebhook = null) {
 		try {
+			// 如果webhook已提供文件信息，直接使用
+			if (filesFromWebhook) {
+				console.log(`使用webhook提供的文件信息，无需API调用`)
+				
+				const files = []
+				
+				// 处理添加的文件
+				filesFromWebhook.added?.forEach(filename => {
+					if (this.isCodeFile(filename)) {
+						files.push({
+							oldPath: null,
+							newPath: filename,
+							diff: null, // webhook不提供diff内容
+							newFile: true,
+							deletedFile: false,
+							renamedFile: false
+						})
+					}
+				})
+				
+				// 处理修改的文件
+				filesFromWebhook.modified?.forEach(filename => {
+					if (this.isCodeFile(filename)) {
+						files.push({
+							oldPath: filename,
+							newPath: filename,
+							diff: null, // webhook不提供diff内容
+							newFile: false,
+							deletedFile: false,
+							renamedFile: false
+						})
+					}
+				})
+				
+				// 处理删除的文件
+				filesFromWebhook.removed?.forEach(filename => {
+					if (this.isCodeFile(filename)) {
+						files.push({
+							oldPath: filename,
+							newPath: null,
+							diff: null, // webhook不提供diff内容
+							newFile: false,
+							deletedFile: true,
+							renamedFile: false
+						})
+					}
+				})
+				
+				return files
+			}
+			
+			// 如果没有webhook文件信息，则通过API获取
 			console.log(`获取提交diff: ${commitSha} in ${owner}/${repo}`)
 
 			const response = await this.client.get(
